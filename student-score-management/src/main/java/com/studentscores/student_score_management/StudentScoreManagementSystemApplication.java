17e11121c19c12b55213f58e44c92c4f7b9692a3
moses olalere <ollysun@gmail.com>
2025-10-23 22:40:49 +0100

    initial commit

---
+++ student-score-management/src/main/java/com/studentscores/student_score_management/StudentScoreManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.studentscores.student_score_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentScoreManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentScoreManagementSystemApplication.class, args);
+	}
+
+}
---
+++ student-score-management/src/test/java/com/studentscores/student_score_management/StudentScoreManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studentscores.student_score_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentScoreManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
